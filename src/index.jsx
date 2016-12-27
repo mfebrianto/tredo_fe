b0b95ff5955f92b3168f303b5917a4bfcb03abb4
michael febrianto <michaelfebrianto@gmail.com>
2016-12-27 20:46:00 +1100

    master : put in store and provider

--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,11 @@ import './assets/stylesheets/main.scss';
 import React from 'react'
 import { render } from 'react-dom'
 
+import { Provider } from 'react-redux';
+import store from './store.js';
+
 
 render((
-  <div>
-    test
-  </div>
+  <Provider store={ store }>
+  </Provider>
 ), document.getElementById('root'));
