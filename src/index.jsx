4914f18ae5d4270a0ad066711f1b4aef5fc903ce
michael febrianto <michaelfebrianto@gmail.com>
2016-12-27 20:42:42 +1100

    master : remove store

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,10 @@ import './assets/stylesheets/main.scss';
 
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
-
-import { Provider } from 'react-redux';
-import store from './store.js';
 
 
 render((
-  <Provider store={ store }>
-    <Router history={hashHistory}>
-
-    </Router>
-  </Provider>
+  <div>
+    test
+  </div>
 ), document.getElementById('root'));
