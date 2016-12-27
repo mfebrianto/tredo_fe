ce4ac6011e2774bd249bad89b0a6c5cdb5593d1f
michael febrianto <michaelfebrianto@gmail.com>
2016-12-28 01:33:40 +1100

    20161228 : put in logo

--- src/components/commons/TopBarMenu.jsx
+++ src/components/commons/TopBarMenu.jsx
@@ -5,28 +5,25 @@ class TopBarMenu extends React.Component {
 
   render(){
     return (
-      <Navbar inverse collapseOnSelect>
+      <Navbar inverse collapseOnSelect className='tredo-navbar'>
         <Navbar.Header>
           <Navbar.Brand>
-            <a href="#">React-Bootstrap</a>
+            <div className='tredo-logo'/>
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
         <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="#">Link</NavItem>
-            <NavItem eventKey={2} href="#">Link</NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.3}>Separated link</MenuItem>
-            </NavDropdown>
+          <Nav className='nav-left'>
+            <NavItem eventKey={1} href="#">Categories</NavItem>
+            <NavItem eventKey={2} href="#">About</NavItem>
           </Nav>
           <Nav pullRight>
-            <NavItem eventKey={1} href="#">Link Right</NavItem>
-            <NavItem eventKey={2} href="#">Link Right</NavItem>
+            <NavItem eventKey={1} href="#" className='login-label'>Log in</NavItem>
+            <NavItem eventKey={2} href="#">
+              <div className='sign-up-label'>
+                Sign up
+              </div>
+            </NavItem>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
